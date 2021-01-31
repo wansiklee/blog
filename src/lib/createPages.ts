bef02ea28cda6b07c30c888b111cfcfc519739e9
wansiklee <dhkstlr2662@gmail.com>
2021-01-31 17:58:52 +0900

    Add create pages test post

--- src/lib/createPages.ts
+++ src/lib/createPages.ts
@@ -1,6 +1,34 @@
 import { CreatePagesArgs } from 'gatsby';
+import path from 'path';
 
-export const createPages = async ({ actions }: CreatePagesArgs) => {
+export const createPages = async ({ actions, graphql }: CreatePagesArgs) => {
   const { createPage } = actions;
-  console.log('typescript!');
+  
+  const { data, errors }:any = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if(errors) throw errors;
+
+  data.allMarkdownRemark.edges.forEach(({ node }: any) => {
+    createPage({
+      path: node.frontmatter.title,
+      context: {
+          html: node.html,
+          title: node.frontmatter.title,
+      },
+      component: path.resolve(__dirname, '../templates/PostTemplate.tsx'),
+    });
+  });
 };
